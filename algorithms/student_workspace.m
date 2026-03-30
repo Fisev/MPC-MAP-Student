c89fbd5902fde745fb40a37bfe5cae733ec5469e
Filip Ševčík <fsielvicpik@gmail.com>
2026-03-30 20:09:36 +0200

    wokring on week 2 tasks - task 1,2,3 amlost done

--- algorithms/student_workspace.m
+++ algorithms/student_workspace.m
@@ -1,6 +1,73 @@
 function [public_vars] = student_workspace(read_only_vars,public_vars)
 %STUDENT_WORKSPACE Summary of this function goes here
 
+numberOfMeassurements = 150;
+global lidar_measurements
+global gnss_measurements
+global i
+
+if isempty(i)
+    i = 1;
+    lidar_measurements = zeros(numberOfMeassurements, 8);
+    gnss_measurements  = zeros(numberOfMeassurements, 2);
+end
+
+if i <= 150
+    lidar_measurements(i, :) = read_only_vars.lidar_distances;
+    gnss_measurements(i, :) = read_only_vars.gnss_position;
+    i = i+1;
+end
+%% Task 2
+if i == 150
+    lidarStandartDev = std(lidar_measurements, 0, "all")
+    lidarChannelsStandartDev(1) = std(lidar_measurements(:,1), 0, "all");
+    lidarChannelsStandartDev(2) = std(lidar_measurements(:,2), 0, "all");
+    lidarChannelsStandartDev(3) = std(lidar_measurements(:,3), 0, "all");
+    lidarChannelsStandartDev(4) = std(lidar_measurements(:,4), 0, "all");
+    lidarChannelsStandartDev(5) = std(lidar_measurements(:,5), 0, "all");
+    lidarChannelsStandartDev(6) = std(lidar_measurements(:,6), 0, "all");
+    lidarChannelsStandartDev(7) = std(lidar_measurements(:,7), 0, "all");
+    lidarChannelsStandartDev(8) = std(lidar_measurements(:,8), 0, "all")
+    
+    gnssStandartDev = std(gnss_measurements, 0, "all")
+    gnssChannelsStandartDev(1) = std(gnss_measurements(:, 1), 0, "all")
+    gnssChannelsStandartDev(2) = std(gnss_measurements(:, 2), 0, "all")
+    
+    figure('Name','LiDAR histograms');
+    for lidarChannel = 1:8
+        subplot(2, 4, lidarChannel);
+        histogram(lidar_measurements(:, lidarChannel), 20);
+        xlabel('Meassurements'); ylabel('Count');
+    end
+
+    figure('Name','Gnss histograms');
+    for gnssChannel = 1:2
+        subplot(1, 2, gnssChannel );
+        histogram(gnss_measurements(:, gnssChannel ), 20);
+    end
+
+end
+
+%% Task 3
+if i == 150
+    lidar_covariance = cov(lidar_measurements)
+    gnss_covariance = cov(gnss_measurements)
+
+    mainDiagLidar = diag(lidar_covariance)
+    mainDiagGnaa = diag(gnss_covariance)
+    
+    lidarChannelsStandartDevSquared = lidarChannelsStandartDev.^2
+    gnssChannelsStandartDevSquared = gnssChannelsStandartDev.^2
+
+
+    i = i+1;
+end
+
+%% Task 4
+
+
+
+%%
 % 8. Perform initialization procedure
 if (read_only_vars.counter == 1)
           
@@ -23,8 +90,6 @@ public_vars.path = plan_path(read_only_vars, public_vars);
 
 % 13. Plan next motion command
 public_vars = plan_motion(read_only_vars, public_vars);
-
-
 
 end
 
