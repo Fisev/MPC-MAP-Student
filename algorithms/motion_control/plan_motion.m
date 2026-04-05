2f87466e0a9d90e151b7f77d658b52611f5b589f
Filip Ševčík <fsielvicpik@gmail.com>
2026-04-05 23:47:21 +0200

    working on task 3

--- algorithms/motion_control/plan_motion.m
+++ algorithms/motion_control/plan_motion.m
@@ -8,13 +8,31 @@ function [public_vars] = plan_motion(read_only_vars, public_vars)
     
     % II. Compute motion vector
     
-    nextWayPoint = public_vars.path(read_only_vars.counter);
-
     pose = read_only_vars.mocap_pose;
     x = pose(1);
     y = pose(2);
     theta = pose(3);
+
+    bestDist = 1000000;
+    N = size(public_vars.path, 1);
+    nextWayPoint = public_vars.path(end,:);
+
+    for i = 1:N
+        dx = public_vars.path(i,1) - x;
+        dy = public_vars.path(i,2) - y;
     
+        Lx =  cos(theta)*dx + sin(theta)*dy;
+        Ly = -sin(theta)*dx + cos(theta)*dy;
+    
+        if Lx > 0
+            d = hypot(Lx, Ly);
+            if d < bestDist
+                bestDist = d;
+                nextWayPoint = public_vars.path(i,:);
+            end
+        end
+    end
+
     dx = nextWayPoint(1) - x;
     dy = nextWayPoint(2) - y;
     
@@ -29,10 +47,17 @@ function [public_vars] = plan_motion(read_only_vars, public_vars)
         curvature = 2 * Ly / (Ld^2);
     end
 
-    
-    v = 0.5;
-    w = v*curvature;
-    w = max(min(w, 1.0), -1.0);
+    vMax = 0.5;
+    wMax = 1.0;
+
+    v = min(vMax, Ld);
+    w = v * curvature;
+    w = max(min(w, wMax), -wMax);
+
+    if Ld < 0.05
+        v = 0;
+        w = 0;
+    end
 
     public_vars.motion_vector = [v, w];
 
