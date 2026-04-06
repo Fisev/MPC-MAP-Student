99c5d3eb8d82cdf6d384114ae508c904157dc4aa
Filip Ševčík <fsielvicpik@gmail.com>
2026-04-06 11:46:50 +0200

    task 3 done

--- algorithms/path_planning/plan_path.m
+++ algorithms/path_planning/plan_path.m
@@ -4,7 +4,7 @@ function [path] = plan_path(read_only_vars, public_vars)
     %% Task 2
     N = 20;
     x1 = (ones(N, 1) * 2);
-    y1 = linspace(1, 9, N)';
+    y1 = linspace(1.1, 9, N)';
     straightLine = [x1, y1];
 
     alpha = linspace(pi/2, pi, N)';
@@ -16,7 +16,7 @@ function [path] = plan_path(read_only_vars, public_vars)
     x3 = 0.5*sin(0.8*y3)+1.5;
     sineWawe = [x3, y3];
  
-    public_vars.path = straightLine;
+    public_vars.path = sineWawe;
 %%
 
     planning_required = 0;
